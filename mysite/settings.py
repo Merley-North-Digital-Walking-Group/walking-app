f70dcfad12c4a4a7c03fd918898d75a5dc5d6a4c
Pol <drifting.gull@gmail.com>
2022-01-31 16:35:52 +0000

    Added initial Walks model - Pol

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'groups',
     'users',
     'reviews',
-    'walks'
+    'walks',
 ]
 
 MIDDLEWARE = [
--- walks/models.py
+++ walks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+# from pandas import to_timedelta
 
 # Create your models here.
+
+class Walk(models.Model):
+    walk_name = models.CharField(max_length=100)
+    walk_length = models.DecimalField(max_digits=4, decimal_places=2)
+    parking = models.BooleanField()
+    wheelchair_access = models.BooleanField()
+    toilet = models.BooleanField()
+    cafe = models.BooleanField()
+    rest_area = models.BooleanField()
+    scenic_spot = models.BooleanField()
+    dog_bin = models.BooleanField()
+    rating = models.IntegerField()
+    photo = models.ImageField()
+    # map_coordinates =
+    # reviews = 
+    # elevation =
+    # step_count = 

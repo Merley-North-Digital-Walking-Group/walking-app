88faf87abe328ec0d2e8137516a46c80222c08f5
vabarnett <vabarnett@yahoo.com>
2022-02-01 11:38:52 +0000

    Adding user models to create user database table

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    #walker id is automatically generated
+    user_type = models.CharField(max_length=20)
+    name_first = models.CharField(max_length=200)
+    name_last = models.CharField(max_length=200)
+    username_handle = models.CharField(max_length=200)
+    gender = models.CharField(max_length=50)
+    DOB = models.DateField()
+    password = models.CharField(max_length=100)
+    #groups = models.ForeignKey("groups", on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)

239b1f3487d522e0ccf3eff20d2af6ce705c3a46
vabarnett <vabarnett@yahoo.com>
2022-02-01 14:04:22 +0000

    Add model to groups

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    group_name = models.CharField(max_length=200)
+    group_description = models.TextField()
+    #group_photo = models.ImageField()
+    meeting_day = models.CharField(max_length=50)
+    meeting_time = models.DateTimeField()
+    meetup_link = models.CharField(max_length=300)
+    #members_list = ForeignKey link to user ids 

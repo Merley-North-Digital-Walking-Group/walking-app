20881fd46b5e82ee34fc2259d96afb3073e63b2a
Pol <drifting.gull@gmail.com>
2022-02-01 16:59:23 +0000

    Added Reviews and Comments models, updated the Walks model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,30 @@
+from datetime import date
+from statistics import mode
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from PIL import Image
+# from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Review(models.Model):
+    # review ID will be automatically created
+    # author = need to connect to user via user/walker ID
+    # walk_ID = need to connect to the walk via walk ID
+    walk_rating = models.IntegerField(default=1, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=3000)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to=None, height_field=768, width_field=1024)
+    approve_count = models.IntegerField()
+    disapprove_count = models.IntegerField()
+
+
+class Comment(models.Model):
+    # comment ID will be automatically created
+    # author = pulled in from walker ID
+    # on_review = pulled in from review ID
+    content = models.TextField(max_length=3000)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField()
+    approve_count = models.IntegerField(upload_to=None, height_field=768, width_field=1024)
+    disapprove_count = models.IntegerField()
--- walks/models.py
+++ walks/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-# from pandas import to_timedelta
+from geoposition.fields import GeopositionField
+from PIL import Image
 
 # Create your models here.
 
 class Walk(models.Model):
+    # walk ID will be automatically generated
     walk_name = models.CharField(max_length=100)
+    map_coordinates = GeopositionField()
     walk_length = models.DecimalField(max_digits=4, decimal_places=2)
+    elevation = models.IntegerField     # Ideally this will be grabbed from the highest point on Google Maps
+    step_count = models.IntegerField()
     parking = models.BooleanField()
     wheelchair_access = models.BooleanField()
     toilet = models.BooleanField()
@@ -14,8 +19,5 @@ class Walk(models.Model):
     scenic_spot = models.BooleanField()
     dog_bin = models.BooleanField()
     rating = models.IntegerField()
-    photo = models.ImageField()
-    # map_coordinates =
-    # reviews = 
-    # elevation =
-    # step_count = 
+    photo = models.ImageField(upload_to=None, height_field=768, width_field=1024)
+    # reviews = need to connect via review IDs

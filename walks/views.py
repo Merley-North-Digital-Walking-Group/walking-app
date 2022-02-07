d185d7f4ac707fbc03668bd5ed05a732cf058e92
vabarnett <vabarnett@yahoo.com>
2022-02-07 13:34:15 +0000

    Adds a link to the database for testing/demonstrating how functionality is linked

--- walks/views.py
+++ walks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Walk #required to import information about the database table called 'Walk' in walks/models 
 
 # Create your views here.
 def walks_list(request):
     return render(request, 'walks/walks_list.html', {})
 
 def walks_detail(request):
-    return render(request, 'walks/walks_detail.html', {})
+    walk_list = Walk.objects.all()#creates an object that refers back to the database table created in models called 'Walk' and assigns it a variable name 'walk_list'
+    return render(request, 'walks/walks_detail.html', {'walk_list': walk_list})# we need to create a dict object here with the variable name assigned above

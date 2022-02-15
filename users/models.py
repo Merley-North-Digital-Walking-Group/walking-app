6d450606f847ca92cbd1672672ba88cf7aa5d895
vabarnett <vabarnett@yahoo.com>
2022-02-15 10:01:27 +0000

    Removes unecessary code from user class

--- users/models.py
+++ users/models.py
@@ -9,13 +9,7 @@ class App_User(models.Model):
     gender = models.CharField(max_length=50)
     location = models.CharField(max_length=100)
     group = models.ManyToManyField('groups.Group')
-    #walker id is automatically generated
-    #user_type = models.CharField(max_length=20)
-    #name_first = models.CharField(max_length=200)
-    #name_last = models.CharField(max_length=200)
-    #username_handle = models.CharField(max_length=200)
-    #password = models.CharField(max_length=100)
-    #email = models.CharField(max_length=200)
+    
    
 
     def __str__(self):

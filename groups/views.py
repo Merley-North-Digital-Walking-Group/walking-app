6996118b8d0cc85b60fc8cfa6f7667c58f8f3eda
evfsmart <evfsmart@gmail.com>
2022-02-15 12:55:20 +0000

    Add group_details page.

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from groups.models import Group
+
 
 # Create your views here.
+def group_details(request, pk):
+    group = get_object_or_404(Group, pk=pk)
+    return render(request, 'groups/group_details.html', {'group': group}) 
+    
+
+    
+    

c3baf186b55aac91b5d7c258d733189e9432ad66
vabarnett <vabarnett@yahoo.com>
2022-02-17 06:41:21 +0000

    changes datatype in group meeting times

--- groups/models.py
+++ groups/models.py
@@ -6,7 +6,7 @@ class Group(models.Model):
     group_description = models.TextField()
     # group_photo = models.ImageField(default=None, height_field=768, width_field=1024)
     meeting_day = models.CharField(max_length=50)
-    meeting_time = models.DateTimeField()
+    meeting_time =  models.CharField(max_length=200, default = "Meeting times may vary, please check with group")
     meetup_link = models.CharField(max_length=300, help_text="Must include 'https://www.' ")
 
     def __str__(self):
